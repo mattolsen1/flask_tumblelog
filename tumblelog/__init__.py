627b525473a418260df722fc3673200692595e03
Matthew Olsen <Matthew.Olsen@demandmedia.com>
2013-03-05 20:34:55 -0800

    more

--- tumblelog/__init__.py
+++ tumblelog/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask.ext.mongoengine import MongoEngine
 
 app = Flask(__name__)
+"""
 app.config["MONGODB_DB"] = "my_tumble_log"
 app.config["MONGODB_USERNAME"] = "molsen"
 app.config["MONGODB_PASSWORD"] = "shauna"
@@ -9,6 +10,13 @@ app.config["MONGODB_HOST"] = "linus.mongohq.com"
 app.config["MONGODB_PORT"] = "10038"
 #app.config["MONGODB_SETTINGS"] = {"DB": "my_tumble_log"}
 #app.config["MONGODB_SETTINGS"] = {'DB': "mongodb://molsen:shauna@linus.mongohq.com:10038/my_tumble_log"}
+app.config["SECRET_KEY"] = "KeepThisS3cr3t"
+"""
+app.config["MONGODB_DB"] = "app12893270"
+app.config["MONGODB_USERNAME"] = "molsen"
+app.config["MONGODB_PASSWORD"] = "shauna"
+app.config["MONGODB_HOST"] = "linus.mongohq.com"
+app.config["MONGODB_PORT"] = "10056"
 app.config["SECRET_KEY"] = "KeepThisS3cr3t"
 
 db = MongoEngine(app)
