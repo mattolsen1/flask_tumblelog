fbcd5486383070173e1d496d8a291a05f2d11e6a
Matthew Olsen <Matthew.Olsen@demandmedia.com>
2013-03-05 19:27:02 -0800

    First Commit of Flask App

---
+++ README.md
@@ -0,0 +1,15 @@
+A Flask Tumblelog
+=================
+
+This Tumblelog uses the Flask web framework, Mongohq for the backend and Heroku for hosting
+
+Installation
+------------
+
+
+
+Run the tumblelog:
+   
+    python manage.py runserver
+
+Goto: [http://localhost:5000](http://localhost:5000)
---
+++ tumblelog/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask.ext.mongoengine import MongoEngine
+
+app = Flask(__name__)
+app.config["MONGODB_DB"] = "my_tumble_log"
+app.config["MONGODB_USERNAME"] = "molsen"
+app.config["MONGODB_PASSWORD"] = "shauna"
+app.config["MONGODB_HOST"] = "linus.mongohq.com"
+app.config["MONGODB_PORT"] = "10038"
+#app.config["MONGODB_SETTINGS"] = {"DB": "my_tumble_log"}
+#app.config["MONGODB_SETTINGS"] = {'DB': "mongodb://molsen:shauna@linus.mongohq.com:10038/my_tumble_log"}
+app.config["SECRET_KEY"] = "KeepThisS3cr3t"
+
+db = MongoEngine(app)
+
+
+def register_blueprints(app):
+    # Prevents circular imports
+    from tumblelog.views import posts
+    from tumblelog.admin import admin
+    app.register_blueprint(posts)
+    app.register_blueprint(admin)
+
+register_blueprints(app)
+
+if __name__ == '__main__':
+    app.run()
